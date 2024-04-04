4d7b6e74a32d6fa191a58896393093c05c3243af
velkee <89312879+Velkee@users.noreply.github.com>
2024-04-04 10:58:24 +0200

    Hostname detection and uuid generation for Authorization header

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
 use std::{collections::HashMap, env};
 
 use reqwest::header::{HeaderMap, AUTHORIZATION};
+use serde::{Deserialize, Serialize};
+use sysinfo::System;
+use uuid::Uuid;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    uuid: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let client = reqwest::Client::new();
 
+    let hostname = match System::host_name() {
+        Some(hostname) => hostname,
+        None => {
+            println!("WARNING: Hostname could not be determined, falling back to OS.");
+            env::consts::OS.to_string()
+        }
+    };
+
+    // TODO: save the uuid somehow to be reused so it doesn't generate 1000 devices in Jellyfin
+    let uuid = Uuid::new_v4();
+
     let mut headers = HeaderMap::new();
     headers.insert(
         AUTHORIZATION,
-        format!(
-            "MediaBrowser Client=\"Dollfish Jellyfin Client\", Device=\"{}\", DeviceId=\"test\", Version=\"0.1.0\", Token=\"\"",
-            env::consts::OS,
-        )
+        format!("MediaBrowser Client=\"Dollfish Jellyfin Client\", Device=\"{hostname}\", DeviceId=\"{uuid}\", Version=\"{VERSION}\", Token=\"\"")
         .parse()
         .unwrap(),
     );
