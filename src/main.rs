3bd6d45cf000404d52dd909ab7f4f298a440ca81
velkee <89312879+Velkee@users.noreply.github.com>
2024-04-05 13:06:10 +0200

    Config file reading and generation

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,12 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-use std::{collections::HashMap, env};
+use std::{
+    collections::HashMap,
+    env,
+    fs::{File, OpenOptions},
+    io::{Read, Write},
+};
 
 use reqwest::header::{HeaderMap, AUTHORIZATION};
 use serde::{Deserialize, Serialize};
@@ -27,6 +32,7 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
+    device_name: String,
     uuid: String,
 }
 
@@ -34,21 +40,53 @@ struct Config {
 async fn main() -> Result<(), reqwest::Error> {
     let client = reqwest::Client::new();
 
-    let hostname = match System::host_name() {
-        Some(hostname) => hostname,
-        None => {
-            println!("WARNING: Hostname could not be determined, falling back to OS.");
-            env::consts::OS.to_string()
+    let mut config_file = match File::open("./dollfish.conf") {
+        Ok(file) => file,
+        Err(_) => {
+            println!("Config doesn't exist, creating with defaults");
+
+            let hostname = match System::host_name() {
+                Some(hostname) => hostname,
+                None => {
+                    println!("WARNING: Hostname could not be determined, falling back to OS.");
+                    env::consts::OS.to_string()
+                }
+            };
+            let uuid = Uuid::new_v4();
+
+            let config = Config {
+                device_name: hostname,
+                uuid: uuid.to_string(),
+            };
+
+            let mut file = OpenOptions::new()
+                .write(true)
+                .read(true)
+                .create(true)
+                .truncate(true)
+                .open("./dollfish.conf")
+                .expect("Could not create config file");
+
+            file.write_all(toml::to_string(&config).unwrap().as_bytes())
+                .expect("Could not write to new config");
+
+            file
         }
     };
 
-    // TODO: save the uuid somehow to be reused so it doesn't generate 1000 devices in Jellyfin
-    let uuid = Uuid::new_v4();
+    let mut config_buffer = String::new();
+    let config = config_file
+        .read_to_string(&mut config_buffer)
+        .expect("Could not read from config")
+        .to_string();
+
+    let config: Config =
+        toml::from_str(&config).expect("Could not convert config from TOML to Struct");
 
     let mut headers = HeaderMap::new();
     headers.insert(
         AUTHORIZATION,
-        format!("MediaBrowser Client=\"Dollfish Jellyfin Client\", Device=\"{hostname}\", DeviceId=\"{uuid}\", Version=\"{VERSION}\", Token=\"\"")
+        format!("MediaBrowser Client=\"Dollfish Jellyfin Client\", Device=\"{}\", DeviceId=\"{}\", Version=\"{}\", Token=\"\"", config.device_name, config.uuid, VERSION)
         .parse()
         .unwrap(),
     );
