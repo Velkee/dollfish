e494bf6384ad242a0fa2c019ba66681e47b9f57c
Fenic <89312879+Velkee@users.noreply.github.com>
2024-08-14 14:30:28 +0200

    Start of rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use reqwest::header::{HeaderMap, AUTHORIZATION};
+
 fn main() {
-    
+    let mut headers = HeaderMap::new(); 
+    headers.insert(AUTHORIZATION, "Client='Dollfish'".parse().unwrap());
+
+    println!("{:#?}", headers)
 }
