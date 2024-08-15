a1a4a83f6d2d12c190aa491a364f6f652c79b447
Fenic <89312879+Velkee@users.noreply.github.com>
2024-08-15 13:21:59 +0200

    Note to self: see if you can get openapi-generator to work

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 use reqwest::header::{HeaderMap, AUTHORIZATION};
 
-fn main() {
-    let mut headers = HeaderMap::new(); 
-    headers.insert(AUTHORIZATION, "Client='Dollfish'".parse().unwrap());
+#[tokio::main]
+async fn main() {
+    let client = reqwest::Client::new();
 
-    println!("{:#?}", headers)
+    let mut headers = HeaderMap::new();
+    headers.insert(AUTHORIZATION, "Client='Dollfish'".parse().unwrap());
+    headers.append(AUTHORIZATION, "Device='hepahestus'".parse().unwrap());
+    headers.append(AUTHORIZATION, "DeviceId='testdevice'".parse().unwrap());
+    headers.append(AUTHORIZATION, "Version='0.1.0'".parse().unwrap());
+
+    let authentication_results = client
+        .post("http://localhost:8096/Users/AuthenticateByName")
+        // .headers(headers)
+        .body("Test")
+        .send()
+        .await
+        .unwrap()
+        .status();
+
+    println!("{}", authentication_results)
 }
